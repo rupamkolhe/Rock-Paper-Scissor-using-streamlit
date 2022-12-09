1ddbae186053fff1b2da578dae0bf94ea0e98e7a
Rupam Kolhe <kolherupam@gmail.com>
2022-12-09 10:31:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
